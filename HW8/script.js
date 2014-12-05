9cb3157eb24b9e06063bf4d1c75942488e800935
evilsunny <aline.abramova@gmail.com>
2014-12-05 15:28:46 +0200

    8

---
+++ HW8/script.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+
+    $('#signUpForm').on('submit',function (event) {
+        event.preventDefault();
+        var signUpForm = $('#signup');
+        var userId = readValue(signUpForm,'login');
+        var userPassword = readValue(signUpForm,'password');
+        var userPasswordConfirmation = readValue(signUpForm,'passwordConfirmation');
+        var params = [];
+        params.push(userId,userPassword,userPasswordConfirmation);
+        console.log(userId,userPassword,userPasswordConfirmation);
+        var successSignUp = function(data){
+            console.log('SUCCESS!');
+            console.log(data);
+        }
+
+        var failureSignUp = function(err){
+            console.log('ERROR!');
+            console.log(err);
+        }
+        sendRequest(params,"SIGNUP", successSignUp,failureSignUp);
+    });
+
+    $('#loginForm').on('submit',function (event) {
+        event.preventDefault();
+        var loginForm = $('#loginForm');
+        var userId = readValue(loginForm,'login');
+        var userPassword = readValue(loginForm,'password');
+        var params = [];
+        params.push(userId,userPassword);
+        var successLogin = function(data){
+            console.log('SUCCESS!');
+            console.log(data);
+        }
+
+        var failureLogin = function(err){
+            console.log('ERROR!');
+            console.log(err);
+        }
+        console.log(userId,userPassword);
+        sendRequest(params,'LOGIN',successLogin,failureLogin);
+
+    });
+
+
+    function sendRequest(params,requestType,success,failure) {
+        var requestData;
+        var requestAddress;
+
+        switch (requestType){
+            case "LOGIN":
+                requestData = {
+                    login: params[0],
+                    password:params[1]
+                }
+                requestAddress = 'login';
+                break;
+            case "SIGNUP":
+                requestData = {
+                    login:params[0],
+                    password:params[1],
+                    passwordConfirmation:params[2]
+                }
+                requestAddress = 'signup';
+                break;
+        }
+
+        $.post("http://api.sudodoki.name:8888/"+requestAddress, {
+            data: requestData
+        }).done(success).error(failure);
+    }
+
+
+    function readValue(valueForm,nameInput) {
+        var value = valueForm.find('input[name = '+nameInput+']').val();
+        console.log(value);
+        return value;
+    }
+});
