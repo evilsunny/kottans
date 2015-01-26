a0705aed52c7dbf904b7d2014cf05e2fde82518b
evilsunny <aline.abramova@gmail.com>
2015-01-27 01:55:05 +0200

    hw8

--- HW8/script.js
+++ HW8/script.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
     $('#errorSignUp').hide();
     $('#show-full').hide();
 
-
     var token;
 
     $('#signUpForm').on('submit',function (event) {
@@ -14,12 +13,14 @@ $(document).ready(function() {
         var userPasswordConfirmation = readValue(signUpForm,'passwordConfirmation');
         var params = [];
         params.push(userId,userPassword,userPasswordConfirmation);
-        console.log(userId,userPassword,userPasswordConfirmation);
+        //console.log(userId,userPassword,userPasswordConfirmation);
         var successSignUp = function(data){
             console.log('SUCCESS!');
-            console.log(data);
-            $('#list').show("slow");
+            console.log(data["token"]);
             token = data["token"];
+            $('#list').show("slow");
+
+
         }
 
         var failureSignUp = function(err){
@@ -30,6 +31,45 @@ $(document).ready(function() {
         sendRequest(params,"SIGNUP", successSignUp,failureSignUp);
     });
 
+    $('#list div').on("click",function(){
+        console.log("CLICKED!");
+        console.log($(this).id());
+    })
+
+    var successGetUser = function(data){
+        console.log(data);
+        $("#list").hide();
+        var data = jQuery.parseJSON(data);
+        var user = data[0].user;
+
+                var html ="<div>";
+                html+='<h2>'+ user.name.title+" "+ user.name.first+" "+user.name.last+'</h2>';
+                html+='<section>';
+                html+='<h3>Location</h3>';
+                html+= 'Street: '+user.location.street+" "+user.location.city+" "+user.location.state+" "+user.location.zip ;
+                html+='</section>';
+
+                html+='<section>';
+                html+='<h3>Connect with him!</h3>';
+                html+='<a href="mailto:'+ user.email + '">'+user.email+'</a>';
+                html+='<br/>';
+                html+='<a href="tel:'+ user.cell +'">'+user.cell+'</a>';
+                html+='</section>';
+                html+='</div>';
+
+
+
+            $("#show-full").html(html);
+            $("#show-full").show("slow");
+
+
+    }
+
+    var errorGetUser = function(err){
+        console.log(err);
+    }
+
+
     $('#loginForm').on('submit',function (event) {
         event.preventDefault();
         var loginForm = $('#loginForm');
@@ -38,29 +78,32 @@ $(document).ready(function() {
         var params = [];
         params.push(userId,userPassword);
         var successLogin = function(data){
-            console.log('SUCCESS!');
             console.log(data);
             $('#login').hide();
             $('#signup').hide();
             $('#list').show("slow");
-            token = data["token"];
+            token = getToken(data);
+            console.log('SUCCESS!');
             var successGetUsers = function(data){
-                console.log(data);
+                //console.log(data);
                 data.forEach(function(item){
-                    var  html = '<li><div class="person '+item.user.gender+' " id = " '+item["id"]+' " ><a class="url n" href="#show-full"><i>'+ item.user.name.title ;
+                    var  html = '<li><div class="person '+item.user.gender+'" id="'+item["id"]+'"><a class="url n" href="#show-full"><i>'+ item.user.name.title ;
                     html += '</i> '+'  '+item.user.name.first +'  '+ item.user.name.last+'</a> </div> </li>';
                     $("#usersList").append(html);
-
-                    $("#"+item.id).on('click',function(){
-                        var successGetUser = function(data){
-                            console.log(data);
-                        }
-                        sendRequest(token,"GETUSER",successGetUser,null);
+                    var  s = (item.id).toString();
+                    $("#"+s).on('click',function(){
+                        sendRequest([item.id],"GETUSER",successGetUser,errorGetUser);
                     });
                 })
 
             }
-            sendRequest(token,"GETUSERS",successGetUsers,null);
+
+
+
+            var errorGetUsers = function(err){
+                console.log(err);
+            }
+            sendRequest(null,"GETUSERS",successGetUsers,errorGetUsers);
 
         }
 
@@ -99,12 +142,25 @@ $(document).ready(function() {
                 TYPE="POST";
                 break;
             case "GETUSERS":
+
                 TYPE = "GET";
-                requestAddress = "users"
+                requestAddress = "users";
+                $.get("http://api.sudodoki.name:8888/" + requestAddress,{},success);
+                //console.log("USERS");
+
                 break;
             case "GETUSER":
                 TYPE = "GET";
-                requestAddress  = "user/"+ params[0]
+                requestAddress  = "user/"+ params[0];
+                $.ajax({
+                        type: "GET",
+                        url: "http://api.sudodoki.name:8888/" + requestAddress,
+                        headers:{"SECRET-TOKEN":token }
+                        //error: failure,
+                        //success:success
+                     }
+                ).done(success).error(failure);
+                break;
         }
 
         if(TYPE === "POST") {
@@ -114,26 +170,25 @@ $(document).ready(function() {
                 data: requestData
                 }).
                 done(success).error(failure);
-        }else{
-            if(TYPE === "GETUSERS"){
-            $.get(
-                "http://api.sudodoki.name:8888/" + requestAddress,
-                {},
-                success
-            );
-        }else{
-                $.ajax({
-                    type:"GET",
-                    url : "http://api.sudodoki.name:8888/" + requestAddress ,
-                    headers: {"SECRET-TOKEN":params[1]};
-            }
         }
-    }
+        //}else{
+        //        $.ajax({
+        //            type:"GET",
+        //            url : "http://api.sudodoki.name:8888/" + requestAddress ,
+        //            headers: {"SECRET-TOKEN":params[1]}});
+        //    }
+       // }
+   }
 
 
     function readValue(valueForm,nameInput) {
         var value = valueForm.find('input[name = '+nameInput+']').val();
-        console.log(value);
+       // console.log(value);
         return value;
+    }
+
+    function getToken(string){
+        var ind = string.indexOf("token");
+        return string.substr(ind+8, 24);
     }
 });
